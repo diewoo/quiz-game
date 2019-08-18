5d523dfb2b652ab4c39bb30bc03168a8a0465d5e
diewoo <dgo250594@gmail.com>
2019-08-18 14:52:20 -0500

    Updates

---
+++ precache-manifest.f081bccd88535db1809d632217682d03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe4b6eb28e372acbf383537e9fa2a72",
+    "url": "/quiz-game/index.html"
+  },
+  {
+    "revision": "d735dee16c6d2c46ec20",
+    "url": "/quiz-game/static/css/main.32c17b14.chunk.css"
+  },
+  {
+    "revision": "49b47d5dab28e66fa08e",
+    "url": "/quiz-game/static/js/2.3c5918aa.chunk.js"
+  },
+  {
+    "revision": "d735dee16c6d2c46ec20",
+    "url": "/quiz-game/static/js/main.09a74f58.chunk.js"
+  },
+  {
+    "revision": "7d7a4ee5b7225a2ae18d",
+    "url": "/quiz-game/static/js/runtime~main.ed733c55.js"
+  }
+]);
